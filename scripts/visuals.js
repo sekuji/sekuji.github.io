d27f49077872ab56639d4aa3c04abfbd91e844af
sekuji <piesik81@gmail.com>
2024-08-05 15:56:48 +0200

    Fixes and changes

--- scripts/visuals.js
+++ scripts/visuals.js
@@ -18,7 +18,11 @@ const darkToggle = document.getElementById('dark-mode-toggle')
  */
 const images = [
   'a1.jpg',
-  'a2.jpg'
+  'a2.jpg',
+  'a3.jpg',
+  'a4.jpg',
+  'a5.jpg',
+  'a6.jpg'
 ];
 
 /**
